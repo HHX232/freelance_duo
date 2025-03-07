edf353769ef64e79b76076efed832815f83fcfd4
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-03-08 01:29:43 +0300

    create default button/link

--- src/components/UI-kit/buttons/FullButton/FullButton.tsx
+++ src/components/UI-kit/buttons/FullButton/FullButton.tsx
@@ -1,21 +1,106 @@
 import {FC} from 'react'
+import {IFullButtonProps} from './FullButton.types'
+import Link from 'next/link'
+import cn from 'clsx'
+import styles from './FullButton.module.scss'
+import arrowSvg from './ButtonIcons/Shape.svg'
+import Image from 'next/image'
+import {ArrowIcon} from './ButtonIcons/ArrowIcon'
 
-type TFullButton = 'Link' | 'Button'
-type TBorderColor =
-  | 'none'
-  | 'orange-light'
-  | 'orange-dark'
-  | 'blue-light'
-  | 'blue-dark'
-  | 'blue-extra-light'
-  | 'gray'
-  | 'dark'
-export interface IFullButtonProps {
-  type?: TFullButton
-  border?: boolean
-  borderColor?: TBorderColor | string
-}
-
-export const FullButton: FC<IFullButtonProps> = () => {
-  return <></>
+export const FullButton: FC<IFullButtonProps> = ({
+  type = 'Button', // Элемент button или Link
+  border = false,
+  borderColor = 'none',
+  borderWidth = '2px',
+  buttonFill = 'none',
+  buttonElementColor = 'black',
+  animationOn = false,
+  butronBorderRadius = '0',
+  disabled = false,
+  href = '/',
+  onClick,
+  buttonText = 'Подобрать квартиру',
+  extraClass,
+  arrowExtraClass,
+  arrowColor = '#000000',
+  arrowWidth = '24',
+  arrowHeight = '24',
+  arrowStrokeWidth = '1.5',
+  arrowExtraStyles,
+  containArrow = false
+}) => {
+  return type === 'Button' ? (
+    <button
+      className={cn(
+        styles.button,
+        {
+          [styles.disabled]: disabled,
+          [styles[`border-color-${borderColor}`]]: borderColor,
+          [styles.border]: border === true,
+          [styles.none_border]: border === false,
+          [styles[`border-width-${borderWidth}`]]: borderWidth,
+          [styles[`button-fill-${buttonFill}`]]: buttonFill,
+          [styles[`button-element-color-${buttonElementColor}`]]: buttonElementColor,
+          [styles.animationOn]: animationOn,
+          [styles[`butron-border-radius-${butronBorderRadius}`]]: butronBorderRadius
+        },
+        extraClass
+      )}
+      onClick={onClick}
+    >
+      {containArrow && (
+        <ArrowIcon
+          extraStyle={arrowExtraStyles}
+          extraClass={cn(styles.arrow, arrowExtraClass)}
+          color={arrowColor}
+          width={arrowWidth}
+          height={arrowHeight}
+          strokeWidth={arrowStrokeWidth}
+        />
+      )}
+      {buttonText}
+      {containArrow && (
+        <ArrowIcon
+          extraStyle={arrowExtraStyles}
+          extraClass={cn(styles.arrow, arrowExtraClass)}
+          color={arrowColor}
+          width={arrowWidth}
+          height={arrowHeight}
+          strokeWidth={arrowStrokeWidth}
+        />
+      )}
+    </button>
+  ) : (
+    <Link
+      className={cn(
+        styles.button,
+        {
+          [styles.disabled]: disabled,
+          [styles.border]: border === true,
+          [styles.none_border]: border === false,
+          [styles[`border-color-${borderColor}`]]: borderColor,
+          [styles[`border-width-${borderWidth}`]]: borderWidth,
+          [styles[`button-fill-${buttonFill}`]]: buttonFill,
+          [styles[`button-element-color-${buttonElementColor}`]]: buttonElementColor,
+          [styles.animationOn]: animationOn,
+          [styles[`butron-border-radius-${butronBorderRadius}`]]: butronBorderRadius
+        },
+        extraClass
+      )}
+      onClick={onClick}
+      href={href}
+    >
+      {buttonText}
+      {containArrow && (
+        <span className={styles.arrow}>
+          <Image className={styles.arrow_image} alt='arrow' width={24} height={24} src={arrowSvg} />
+        </span>
+      )}
+      {containArrow && (
+        <span className={styles.arrow_box}>
+          <Image className={styles.arrow_image} alt='arrow' width={24} height={24} src={arrowSvg} />
+        </span>
+      )}
+    </Link>
+  )
 }
