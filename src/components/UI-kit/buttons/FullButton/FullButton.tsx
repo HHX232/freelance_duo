8c8250884cdcd91d1c0436bba11dc72309b871ea
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-03-11 23:21:37 +0300

    create disabled desctop buttons

--- src/components/UI-kit/buttons/FullButton/FullButton.tsx
+++ src/components/UI-kit/buttons/FullButton/FullButton.tsx
@@ -44,8 +44,18 @@ export const FullButton: FC<IFullButtonProps> = ({
             activeButton && buttonFill === 'none' && borderColor === 'orange-default',
           [styles.active_button_transparent_with_gray_border]:
             activeButton && buttonFill === 'none' && borderColor === 'gray',
-          // ! переделать дизейбл
+          // ! Стили для дизейбл версий
           [styles.disabled]: disabled,
+          [styles.disabled_active_button_bronze]: disabled && activeButton && buttonFill === 'bronze-500',
+          [styles.disabled_active_button_orange]: disabled && activeButton && buttonFill === 'orange-500',
+          [styles.disabled_active_button_white]: disabled && activeButton && buttonFill === 'white',
+          [styles.disabled_active_button_transparent_with_orange_border]:
+            disabled && activeButton && buttonFill === 'none' && borderColor === 'orange-default',
+          [styles.disabled_active_button_transparent_with_gray_border]:
+            disabled && activeButton && buttonFill === 'none' && borderColor === 'gray',
+          [styles.disabled_active_button_gray_transparent]:
+            disabled && activeButton && !border && buttonFill === 'gray-light',
+          // ! Ниже идет кастомные отдельные части кнопок
           [styles[`border-color-${borderColor}`]]: borderColor,
           [styles.border]: border === true,
           [styles.none_border]: border === false,
@@ -54,7 +64,7 @@ export const FullButton: FC<IFullButtonProps> = ({
           [styles[`button-element-color-${buttonElementColor}`]]: buttonElementColor,
           [styles.animationOn]: animationOn,
           [styles[`butron-border-radius-${buttonBorderRadius}`]]: buttonBorderRadius,
-          // задать так же цвета элементам
+          //! Ниже идут с заливкой
           [styles.active_button_bronze]: activeButton && buttonFill === 'bronze-500',
           [styles.active_button_orange]: activeButton && buttonFill === 'orange-500',
           [styles.active_button_white]: activeButton && buttonFill === 'white'
@@ -66,7 +76,7 @@ export const FullButton: FC<IFullButtonProps> = ({
       {containArrow && (
         <ArrowIcon
           extraStyle={arrowExtraStyles}
-          extraClass={cn(styles.arrow, arrowExtraClass)}
+          extraClass={cn(styles.arrow, styles.arrow_active, arrowExtraClass)}
           color={arrowColor}
           width={arrowWidth}
           height={arrowHeight}
@@ -78,7 +88,7 @@ export const FullButton: FC<IFullButtonProps> = ({
       {containArrow && (
         <ArrowIcon
           extraStyle={arrowExtraStyles}
-          extraClass={cn(styles.arrow, arrowExtraClass)}
+          extraClass={cn(styles.arrow, styles.arrow_active, arrowExtraClass)}
           color={arrowColor}
           width={arrowWidth}
           height={arrowHeight}
