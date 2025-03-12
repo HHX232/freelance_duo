97008c4e37eef598e87bd94b3f178c56d3887bfb
Stanislav Smirnov <8268363@gmail.com>
2025-03-13 00:20:33 +0300

    header menu contacts: componentize message button with ui-kit, refactored phone button

--- src/components/shared/page/header/components/HeaderMenu/HeaderMenuBottom/buttons/HeaderMenuBottomPhoneButton/HeaderMenuBottomPhoneButton.tsx
+++ src/components/shared/page/header/components/HeaderMenu/HeaderMenuBottom/buttons/HeaderMenuBottomPhoneButton/HeaderMenuBottomPhoneButton.tsx
@@ -1,22 +1,19 @@
 import {FullButton} from '@src/components/UI-kit/buttons/FullButton/FullButton'
-import styles from './HeaderMenuBottomPhoneButton.module.scss'
 import PhoneSVG from '@icon/phone.svg'
 
 export default function HeaderMenuBottomPhoneButton() {
   return (
-    <div className={styles.phoneButton}>
-      <FullButton
-        type={'Link'}
-        href={'tel:+71231234567'}
-        activeButton={true}
-        border={false}
-        borderColor={''}
-        buttonFill={'none'}
-        buttonElementColor={'white'}
-        buttonText={''}
-      >
-        <PhoneSVG />
-      </FullButton>
-    </div>
+    <FullButton
+      type={'Link'}
+      href={'tel:+71231234567'}
+      activeButton={true}
+      border={false}
+      borderColor={''}
+      buttonFill={'none'}
+      buttonElementColor={'white'}
+      buttonText={''}
+    >
+      <PhoneSVG />
+    </FullButton>
   )
 }
