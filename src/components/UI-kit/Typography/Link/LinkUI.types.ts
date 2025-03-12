76b4ec47534df3077045de498b1c152059de7234
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-03-13 02:23:30 +0300

    createcheck-box

--- src/components/UI-kit/Typography/Link/LinkUI.types.ts
+++ src/components/UI-kit/Typography/Link/LinkUI.types.ts
@@ -6,10 +6,11 @@ type TLinkWeight = 'light' | 'regular' | 'medium' | 'bold'
 
 type AnchorProps = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>
 
-export interface ILinkUIProps extends LinkProps, AnchorProps {
+export interface ILinkUIProps extends Omit<LinkProps, 'href'>, AnchorProps {
   children: ReactNode
   size: TLinkSize
   weight?: TLinkWeight
+  href?: string
   extraClass?: string
   extraStyle?: React.CSSProperties
 }
--- src/components/shared/page/header/components/TextLink/TextLink.tsx
+++ src/components/shared/page/header/components/TextLink/TextLink.tsx
@@ -6,7 +6,7 @@ import {ITextLinkProps} from './TextLink.types'
 export default function TextLink({href, title, style, onClick}: ITextLinkProps) {
   return (
     <div onClick={onClick} className={styles.link} style={style}>
-      <Link href={href}>{title}</Link>
+      {href.length > 0 ? <Link href={href}>{title}</Link> : <span>{title}</span>}
     </div>
   )
 }
