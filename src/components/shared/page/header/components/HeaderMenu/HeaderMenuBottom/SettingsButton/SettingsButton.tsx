e66c78fa67a9a26d6c434ad36ea5cb44ba82fc1e
Stanislav Smirnov <8268363@gmail.com>
2025-03-13 00:05:24 +0300

    refactored settings button in header menu used ui-kit

--- src/components/shared/page/header/components/HeaderMenu/HeaderMenuBottom/SettingsButton/SettingsButton.tsx
+++ src/components/shared/page/header/components/HeaderMenu/HeaderMenuBottom/SettingsButton/SettingsButton.tsx
@@ -1,11 +1,20 @@
-import Link from 'next/link'
 import styles from './SettingsButton.module.scss'
 import SettingsSVG from '@icon/Menu point.svg'
+import {FullButton} from '@src/components/UI-kit/buttons/FullButton/FullButton'
 
 export default function SettingsButton() {
   return (
-    <Link href={'/planirovki-i-ceny'} className={styles.settingsButton}>
+    <FullButton
+      type={'Link'}
+      href={'/planirovki-i-cen'}
+      extraClass={styles.settingsButton}
+      activeButton={true}
+      border={false}
+      borderColor={''}
+      buttonFill={'bronze-500'}
+      buttonText={''}
+    >
       <SettingsSVG />
-    </Link>
+    </FullButton>
   )
 }
