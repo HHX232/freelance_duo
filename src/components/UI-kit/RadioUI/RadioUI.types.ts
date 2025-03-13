697d91bc89618dbdae31143ed7026c46c616020c
Stanislav Smirnov <8268363@gmail.com>
2025-03-13 17:54:02 +0300

    updated RadioUI: replace label field by children

--- src/components/UI-kit/RadioUI/RadioUI.types.ts
+++ src/components/UI-kit/RadioUI/RadioUI.types.ts
@@ -2,7 +2,7 @@ import {ComponentPropsWithRef} from 'react'
 import {TLinkSize} from '../Typography/Link/LinkUI.types'
 
 export interface RadioUIProps extends ComponentPropsWithRef<'input'> {
-  label?: React.ReactNode
+  children?: React.ReactNode
   error?: string
   uiSize?: TLinkSize
   extraClass?: string
