15551cc978a2bebaca1f5de954e6cb72a0a99845
stanislavsmirnov <s.mironov@q747.tech>
2025-03-14 04:49:14 +0100

    TR-AC: попап модалки для транспортной доступности + верстка

--- src/components/pages/transport/TransportWrapper.tsx
+++ src/components/pages/transport/TransportWrapper.tsx
@@ -1,20 +1,27 @@
+"use client"
 import styles from './TransportWrapper.module.scss'
 import Head from '@pages/transport/components/head/Head'
 import Information from '@pages/transport/components/information/Information'
+// import TransportMap from '@pages/transport/components/transportMap/transportMap'
 import Infoblock from '@pages/transport/components/infoblock/Infoblock'
 import Blocks from '@pages/transport/components/blocks/Blocks'
 import Subscription from '@pages/transport/components/subscription/Subscription'
+import TransportModal from '@pages/transport/popup/transportModal'
+import { useState } from 'react'
 
 const TransportWrapper = () => {
+  const [modalView, setModalView] = useState(0);
   return (
     <div className={styles['wrapper']}>
       <Head />
       <Information />
+      {/* <TransportMap /> */}
       <div className={styles.transport_content}>
         <Infoblock />
-        <Blocks />
+        <Blocks setShowModal={setModalView}/>
         <Subscription />
       </div>
+      <TransportModal shown={modalView} onClose={() => setModalView(0)} scrollPage={(page) => setModalView(page)}/>
     </div>
   )
 }
