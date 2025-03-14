1b5b84e55322cda6d2e3aaaf83285e3aea56a719
Redwi-v <>
2025-03-14 20:09:28 +0300

    update

--- src/components/pages/payment-methods/components/ipoteka/ipoteka.tsx
+++ src/components/pages/payment-methods/components/ipoteka/ipoteka.tsx
@@ -4,7 +4,9 @@ import FilledButton from '@shared/filledButton/FilledButton'
 import BorderedButton from '@shared/borderedButton/BorderedButton'
 import styles from '../../payment-methods.module.scss'
 import clsx from 'clsx'
+import ipotekaStyles from './ipoteka.module.scss'
 import {FC} from 'react'
+import Image from 'next/image'
 
 interface IIpotekaTabProps {
   setShownRequestCallBack: (flag: boolean) => void
@@ -13,33 +15,40 @@ interface IIpotekaTabProps {
 export const IpotekaTab: FC<IIpotekaTabProps> = ({setShownRequestCallBack}) => {
   return (
     <>
-      <section
-        className={styles.poster}
-        style={{
-          backgroundImage: `linear-gradient(90deg, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0) 119.89%), url("/content/payment-methods/ipoteka.jpeg")`
-        }}
-      >
-        <h2 className={styles.posterTitle}>Ипотека</h2>
-        <p className={styles.subtitle}>Выгодные условия для вашего дома</p>
+      <div className={ipotekaStyles.container}>
+        <section className={clsx(styles.poster, ipotekaStyles.wrapper)}>
+          <Image
+            className={ipotekaStyles.bg}
+            alt='ipoteka'
+            quality={90}
+            fill
+            src={'/content/payment-methods/ipoteka.jpeg'}
+          />
+          <div className={ipotekaStyles.overflow} />
+          <h2 className={styles.posterTitle}>Ипотека</h2>
+          <p className={styles.subtitle}>Выгодные условия для вашего дома</p>
 
-        <div className={styles.cards}>
-          <div className={styles.card}>
-            <div className={styles['icon-wrapper']}>
-              <FastForwardSVG />
+          <div className={ipotekaStyles.cards_wrapper}>
+            <div className={clsx(styles.cards, ipotekaStyles.cards_list)}>
+              <div className={styles.card}>
+                <div className={styles['icon-wrapper']}>
+                  <FastForwardSVG />
+                </div>
+                <h3 className={styles['card-title']}>Быстрое оформление и экономия средств</h3>
+              </div>
+              <div className={styles.card}>
+                <div className={styles['icon-wrapper']}>
+                  <MonitorSVG />
+                </div>
+                <h3 className={styles['card-title']}>решение по ипотеке, не выходя из дома</h3>
+              </div>
             </div>
-            <h3 className={styles['card-title']}>Быстрое оформление и экономия средств</h3>
           </div>
-          <div className={styles.card}>
-            <div className={styles['icon-wrapper']}>
-              <MonitorSVG />
-            </div>
-            <h3 className={styles['card-title']}>решение по ипотеке, не выходя из дома</h3>
-          </div>
+        </section>
+        <div className={styles['poster-meta']}>
+          <p className={styles['poster-meta__title']}>Есть вопросы? Свяжитесь с нами!</p>
+          <button className={clsx([styles['tab-button']])}>Получить консультацию</button>
         </div>
-      </section>
-      <div className={styles['poster-meta']}>
-        <p className={styles['poster-meta__title']}>Есть вопросы? Свяжитесь с нами!</p>
-        <button className={clsx([styles['tab-button']])}>Получить консультацию</button>
       </div>
 
       <section className={styles.variants}>
@@ -58,12 +67,12 @@ export const IpotekaTab: FC<IIpotekaTabProps> = ({setShownRequestCallBack}) => {
                     <span>по 2 документам</span>
                     <br /> за 5 минут
                   </h3>
-                  <p className={clsx(styles['meta__info-inner__caption'])} style={{color: '#747679'}}>
+                  <p className={clsx(styles['meta__info-inner__caption'])} style={{color: '#555'}}>
                     Бесплатная консультация с кредитным специалистом по номеру
                   </p>
                   <div>
                     <p className={clsx(styles['meta__info-inner__phone'])}>8 (812) 660–82–71</p>
-                    <p className={clsx(styles['meta__info-inner__caption'])} style={{color: '#A0A3A8', marginTop: 8}}>
+                    <p className={clsx(styles['meta__info-inner__caption'])} style={{color: '#A0A3A8', marginTop: 10}}>
                       ежедневно с 9:00 до 18:00
                     </p>
                   </div>
