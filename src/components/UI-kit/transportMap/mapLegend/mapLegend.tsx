a4b5f95c1c17ee2e5c55921137a5c0dae097bf4f
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-03-20 02:45:22 +0300

    build success

--- src/components/UI-kit/transportMap/mapLegend/mapLegend.tsx
+++ src/components/UI-kit/transportMap/mapLegend/mapLegend.tsx
@@ -1,49 +1,50 @@
 import styles from './mapLegend.module.scss'
-import SwitchUI from '@src/components/UI-kit/SwitchUI/SwitchUI'
 import MapPoint from '@shared/mapPoint/mapPoint'
-import { FC, ChangeEvent, useState } from 'react'
+import {FC, ChangeEvent, useState} from 'react'
+import SwitchUI from '../../BaseControls/SwitchUI/SwitchUI'
 
 interface IMapLegend {
-    switchVisibility: (visible: boolean) => void
+  switchVisibility: (visible: boolean) => void
 }
 
 const MapLegend: FC<IMapLegend> = ({switchVisibility}) => {
+  const [isChecked, setIsChecked] = useState(true)
 
-    const [isChecked, setIsChecked] = useState(true);
+  const handleSwitch = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log('CLC-1->', event.target.checked)
+    setIsChecked(event.target.checked)
+    switchVisibility(event.target.checked)
+  }
 
-    const handleSwitch = (event: ChangeEvent<HTMLInputElement>) => {
-        console.log('CLC-1->', event.target.checked);
-        setIsChecked(event.target.checked)
-        switchVisibility(event.target.checked)
-    }
-
-    //'coast' | 'kindergarden' | 'mall' | 'medicine' | 'park' | 'restorant' | 'school' | 'sport' | 'store'
-    const legendItems = [
-        {icon: 'school', text: 'Школы'},
-        {icon: 'kindergarden', text: 'Детские сады'},
-        {icon: 'medicine', text: 'Медицина'},
-        {icon: 'sport', text: 'Спорт'},
-        {icon: 'store', text: 'Магазины'},
-        {icon: 'mall', text: 'ТРК'},
-        {icon: 'restorant', text: 'Рестораны'},
-        {icon: 'park', text: 'Парки'},
-        {icon: 'coast', text: 'Набережная'}
-    ]
+  //'coast' | 'kindergarden' | 'mall' | 'medicine' | 'park' | 'restorant' | 'school' | 'sport' | 'store'
+  const legendItems = [
+    {icon: 'school', text: 'Школы'},
+    {icon: 'kindergarden', text: 'Детские сады'},
+    {icon: 'medicine', text: 'Медицина'},
+    {icon: 'sport', text: 'Спорт'},
+    {icon: 'store', text: 'Магазины'},
+    {icon: 'mall', text: 'ТРК'},
+    {icon: 'restorant', text: 'Рестораны'},
+    {icon: 'park', text: 'Парки'},
+    {icon: 'coast', text: 'Набережная'}
+  ]
   return (
     <section className={styles['maplegend-wrapper']}>
-        <div className={styles['maplegend-container']}>
-            <SwitchUI checked={isChecked} onChange={handleSwitch}/>
-            <p className={styles['maplegend-name']}>Инфраструктура</p>
-        </div>
-        <span className={styles['maplegend-info-icon']}>i</span>
-        <div className={styles['maplegend-legend']}>
-            {legendItems.map(item => {
-                return <div key={item.text} className={styles['maplegend-legend-item']}>
-                    <MapPoint icon={item.icon} />
-                    <p>{item.text}</p>
-                </div>
-            })}
-        </div>
+      <div className={styles['maplegend-container']}>
+        <SwitchUI checked={isChecked} onChange={handleSwitch} />
+        <p className={styles['maplegend-name']}>Инфраструктура</p>
+      </div>
+      <span className={styles['maplegend-info-icon']}>i</span>
+      <div className={styles['maplegend-legend']}>
+        {legendItems.map((item) => {
+          return (
+            <div key={item.text} className={styles['maplegend-legend-item']}>
+              <MapPoint icon={item.icon} />
+              <p>{item.text}</p>
+            </div>
+          )
+        })}
+      </div>
     </section>
   )
 }
--- src/components/UI-kit/transportMap/mobilePopup/mobilePopup.tsx
+++ src/components/UI-kit/transportMap/mobilePopup/mobilePopup.tsx
@@ -1,11 +1,11 @@
 import {Drawer} from 'antd'
 import {FC} from 'react'
 import styles from './mobilePopup.module.scss'
-import {CloseButton} from '@shared/close-button'
 import {YMaps, Map, Placemark, GeoObject, Clusterer} from '@pbe/react-yandex-maps'
 import MapSidebar from '../mapSidebar/mapSidebar'
-import { mapPoi, mapRoutes } from '@src/lib/utils/catalog/mapMockData'
+import {mapPoi, mapRoutes} from '@src/lib/utils/catalog/mapMockData'
 import MapPoint from '@shared/mapPoint/mapPoint'
+import {CloseButton} from '../../BaseControls/buttons/close-button'
 
 interface ITransportModalProps {
   shown: boolean
@@ -13,18 +13,17 @@ interface ITransportModalProps {
 }
 
 const MobilePopup: FC<ITransportModalProps> = ({shown, onClose}) => {
-
-    const legendItems = [
-        {icon: 'school', text: 'Школы'},
-        {icon: 'kindergarden', text: 'Детские сады'},
-        {icon: 'medicine', text: 'Медицина'},
-        {icon: 'sport', text: 'Спорт'},
-        {icon: 'store', text: 'Магазины'},
-        {icon: 'mall', text: 'ТРК'},
-        {icon: 'restorant', text: 'Рестораны'},
-        {icon: 'park', text: 'Парки'},
-        {icon: 'coast', text: 'Набережная'}
-    ]
+  const legendItems = [
+    {icon: 'school', text: 'Школы'},
+    {icon: 'kindergarden', text: 'Детские сады'},
+    {icon: 'medicine', text: 'Медицина'},
+    {icon: 'sport', text: 'Спорт'},
+    {icon: 'store', text: 'Магазины'},
+    {icon: 'mall', text: 'ТРК'},
+    {icon: 'restorant', text: 'Рестораны'},
+    {icon: 'park', text: 'Парки'},
+    {icon: 'coast', text: 'Набережная'}
+  ]
 
   return (
     <Drawer
@@ -46,72 +45,74 @@ const MobilePopup: FC<ITransportModalProps> = ({shown, onClose}) => {
       </div>
 
       <div className={styles.mobile_content}>
-        <YMaps query={{lang: "ru_RU", apikey: "f4f9faf3-0ce8-4dd2-9b67-7843cfeff30f"}}>
-            <Map
-                defaultState={{center: [59.999685, 29.746311], zoom: 14, controls: []}}
-                defaultOptions={{}}
-                options={{ suppressMapOpenBlock: true }}
-                controls={[]}
-                modules={[
-                  "control.FullscreenControl",
-                  "control.SearchControl",
-                  "control.RoutePanel"
-                ]}
-                className={styles.mobile_map}
+        <YMaps query={{lang: 'ru_RU', apikey: 'f4f9faf3-0ce8-4dd2-9b67-7843cfeff30f'}}>
+          <Map
+            defaultState={{center: [59.999685, 29.746311], zoom: 14, controls: []}}
+            defaultOptions={{}}
+            options={{suppressMapOpenBlock: true}}
+            controls={[]}
+            modules={['control.FullscreenControl', 'control.SearchControl', 'control.RoutePanel']}
+            className={styles.mobile_map}
+          >
+            <Clusterer
+              options={{
+                preset: 'islands#invertedVioletClusterIcons',
+                groupByCoordinates: true, // Группировать точки с одинаковыми координатами
+                clusterDisableClickZoom: false, // Разрешить зум на кластере
+                clusterHideIconOnBalloonOpen: false,
+                geoObjectHideIconOnBalloonOpen: false
+              }}
             >
-              <Clusterer 
-                options={{
-                  preset: "islands#invertedVioletClusterIcons",
-                  groupByCoordinates: true, // Группировать точки с одинаковыми координатами
-                  clusterDisableClickZoom: false, // Разрешить зум на кластере
-                  clusterHideIconOnBalloonOpen: false,
-                  geoObjectHideIconOnBalloonOpen: false,
-                }}
-              >
-                {mapPoi && mapPoi.map((place, index) => {
-                    return <Placemark
-                    key={index}
-                    geometry={place.coords}
-                    properties={{ hintContent: place.name, balloonContentBody: place.name }}
-                    modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
-                    options={{
-                      iconLayout: "default#image",
-                      iconImageHref: `/map/icons/${place.icon}`,
-                      iconImageSize: /кронфорт/gi.test(place.name) ? [60, 60] : [30, 30],
-                    }}
-                  />
+              {mapPoi &&
+                mapPoi.map((place, index) => {
+                  return (
+                    <Placemark
+                      key={index}
+                      geometry={place.coords}
+                      properties={{hintContent: place.name, balloonContentBody: place.name}}
+                      modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+                      options={{
+                        iconLayout: 'default#image',
+                        iconImageHref: `/map/icons/${place.icon}`,
+                        iconImageSize: /кронфорт/gi.test(place.name) ? [60, 60] : [30, 30]
+                      }}
+                    />
+                  )
                 })}
-              </Clusterer>
+            </Clusterer>
 
-                {mapRoutes && mapRoutes.map((route, index) => (
-                  <GeoObject
-                    key={index}
-                    geometry={{
-                      type: "LineString",
-                      coordinates: route.points,
-                    }}
-                    options={{
-                      strokeColor: route.color,
-                      strokeWidth: route.lineWidth,
-                      strokeStyle: "dash", // Пунктирная линия
-                    }}
-                  />
-                ))}
-            </Map>
+            {mapRoutes &&
+              mapRoutes.map((route, index) => (
+                <GeoObject
+                  key={index}
+                  geometry={{
+                    type: 'LineString',
+                    coordinates: route.points
+                  }}
+                  options={{
+                    strokeColor: route.color,
+                    strokeWidth: route.lineWidth,
+                    strokeStyle: 'dash' // Пунктирная линия
+                  }}
+                />
+              ))}
+          </Map>
         </YMaps>
         <div className={styles.mobile_infoblock}>
-            <div className={styles.infoblock_line}></div>
-            <h4 className={styles.infoblock_title}>ОБОЗНАЧЕНИЯ</h4>
-            <MapSidebar isOpen={true} isMobile/>
-            <h4 className={styles.infoblock_title}>ИНФРАСТРУКТУРА</h4>
-            <div className={styles['maplegend-legend']}>
-            {legendItems.map(item => {
-                return <div key={item.text} className={styles['maplegend-legend-item']}>
-                    <MapPoint icon={item.icon} />
-                    <p>{item.text}</p>
+          <div className={styles.infoblock_line}></div>
+          <h4 className={styles.infoblock_title}>ОБОЗНАЧЕНИЯ</h4>
+          <MapSidebar isOpen={true} isMobile />
+          <h4 className={styles.infoblock_title}>ИНФРАСТРУКТУРА</h4>
+          <div className={styles['maplegend-legend']}>
+            {legendItems.map((item) => {
+              return (
+                <div key={item.text} className={styles['maplegend-legend-item']}>
+                  <MapPoint icon={item.icon} />
+                  <p>{item.text}</p>
                 </div>
+              )
             })}
-        </div>
+          </div>
         </div>
       </div>
     </Drawer>
