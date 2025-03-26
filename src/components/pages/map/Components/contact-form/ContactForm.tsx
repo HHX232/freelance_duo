560d5279eca511aa9c344c654e06d6e581c9258b
HHX232 <140748811+HHX232@users.noreply.github.com>
2025-03-26 16:05:05 +0300

    fixed: form width on home page

--- src/components/pages/map/Components/contact-form/ContactForm.tsx
+++ src/components/pages/map/Components/contact-form/ContactForm.tsx
@@ -4,6 +4,7 @@ import InputTextUI from '@src/components/UI-kit/BaseControls/inputs/InputTextUI/
 import Link from 'next/link'
 import InputPhoneUI from '@src/components/UI-kit/BaseControls/inputs/InputPhoneUI/InputPhoneUI'
 import clsx from 'clsx'
+import ParagraphUI from '@src/components/UI-kit/Text-Elements/Typography/Paragraph/Paragraph'
 
 const ContactFormPage = () => {
   return (
@@ -12,10 +13,12 @@ const ContactFormPage = () => {
         <div className={styles['info']}>
           <div className={styles.wrapper}>
             <h3 className={styles.title}>Узнайте больше</h3>
-            <p className={styles.description}>
+            <ParagraphUI extraClass={styles.extra_subtitle} size={'md'} weight={'light'}>
               Оставьте заявку на обратный звонок и персональный менеджер свяжется с вами, для уточнения деталей
-            </p>
-            <form>
+            </ParagraphUI>
+            {/* <p className={styles.description}>
+            </p> */}
+            <form className={styles.form_width}>
               <div className={styles['input-wrapper']}>
                 <InputTextUI icon={<></>} theme={'white'} labelText={'Имя'} placeholder='Введите имя' />
               </div>
@@ -35,6 +38,10 @@ const ContactFormPage = () => {
                 buttonBorderRadius={'6px'}
                 extraClass={styles['button-send']}
               />
+              {/* <ParagraphUI extraClass={styles.extra_caption} size={'sm'} weight='light'>
+                Нажимая кнопку «Отправить», вы даёте согласие на{' '}
+                <Link href='/consent'>обработку своих персональных данных</Link>
+              </ParagraphUI> */}
               <p className={styles.caption}>
                 Нажимая кнопку «Отправить», вы даёте согласие на{' '}
                 <Link href='/consent'>обработку своих персональных данных</Link>
