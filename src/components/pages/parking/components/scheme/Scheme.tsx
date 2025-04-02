fa96774aeb29c85056c9bbb20a00a8520711a237
Baglan <b.muratbek@q747.tech>
2025-04-02 15:06:12 +0000

    Feat/parking animations

--- src/components/pages/parking/components/scheme/Scheme.tsx
+++ src/components/pages/parking/components/scheme/Scheme.tsx
@@ -6,21 +6,48 @@ import SchemeDesctop from '@icon/scheme_desctop.svg'
 import SchemeXXXL from '@icon/scheme_xxxl.svg'
 import {useIsLg, useIsMinWidth} from '@utils/useIsMobile'
 import DownloadButton from '@src/components/UI-kit/BaseControls/buttons/old/downloadButton'
+import {useEffect, useRef, useState} from 'react'
 
 const Scheme = () => {
   const isLg = useIsMinWidth(1024)
   const isXXXL = useIsMinWidth(1920)
   const isXl = useIsLg()
 
+
+  const sectionRef = useRef<HTMLDivElement | null>(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true)
+        }
+      },
+      { threshold: 0.3 } // Анимация сработает, когда 30% блока появится в зоне видимости
+    )
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current)
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current)
+      }
+    }
+  }, [])
+
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.info}>
+    <div className={styles.wrapper} ref={sectionRef}>
+      <div className={`${styles.info} ${isVisible ? styles.visible: ''}`}>
         <div className={styles.title}>Схема типового этажа</div>
         <div className={styles['download-wrapper']}>
           <DownloadButton text={'Скачайте схему паркинга'} />
         </div>
       </div>
-      <div className={styles.schemeWrapper}>
+      <div className={`${styles.schemeWrapper} ${isVisible ? styles.visible: ''}`}>
         {isXl ? (
           isLg ? (
             <SchemeIcon className={styles.icon} />
