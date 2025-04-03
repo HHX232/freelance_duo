d913792a2e8ad67af5adb12f49ce50bdcfd89d53
Baglan <b.muratbek@q747.tech>
2025-04-03 08:41:58 +0000

    add commerce animation

--- src/components/pages/commerce/components/head/Head.tsx
+++ src/components/pages/commerce/components/head/Head.tsx
@@ -1,6 +1,7 @@
 'use client'
 import styles from './Head.module.scss'
 import Breadcrumbs from '@src/components/UI-kit/Navigation/Breadcrumbs/Breadcrumbs'
+import {useEffect, useRef, useState} from 'react'
 
 const Head = () => {
   const breadcrumbItems = [
@@ -11,12 +12,37 @@ const Head = () => {
     }
   ]
 
+  const sectionRef = useRef<HTMLDivElement | null>(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true)
+        }
+      },
+      { threshold: 0.3 }
+    )
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current)
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(sectionRef.current)
+      }
+    }
+  }, [])
+
   return (
-    <section className={`${styles['head-wrapper']}`}>
+    <section className={`${styles['head-wrapper']}`} ref={sectionRef}>
       <div>
         <Breadcrumbs items={breadcrumbItems} darkTheme={true} className={styles['router']} />
       </div>
-      <div className={`${styles['text-wrapper']}`}>
+      <div className={`${styles['text-wrapper']} ${ isVisible ? styles.visible : ''}`}>
         <div className={styles.title}>
           Коммерческие <br /> помещения
         </div>
--- src/components/pages/commerce/components/rent/MakeTicket.tsx
+++ src/components/pages/commerce/components/rent/MakeTicket.tsx
@@ -2,7 +2,7 @@
 import styles from './MakeTicket.module.scss'
 import {useIsMaxWidth, useIsMinWidth} from '@utils/useIsMobile'
 import TicketForm from '@pages/commerce/components/rent/form/TicketForm'
-import {FC} from 'react'
+import {FC, useEffect, useRef, useState} from 'react'
 
 interface MakeTicketProps {
   openModal: () => void
@@ -14,10 +14,35 @@ const MakeTicket: FC<MakeTicketProps> = ({openModal}) => {
   const isXXl = useIsMinWidth(1600)
   const isNotBr = (isMinMD && isMaxLg) || isXXl
 
+  const sectionRef = useRef<HTMLDivElement | null>(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true)
+        }
+      },
+      { threshold: 0.3 }
+    )
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current)
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(sectionRef.current)
+      }
+    }
+  }, [])
+
   return (
-    <div className={styles.rentWrapper}>
+    <div className={styles.rentWrapper} ref={sectionRef}>
       <div className={styles.innerWrapper}>
-        <div className={styles.title}>
+        <div className={`${styles.title} ${isVisible ? styles.visible : ""}`}>
           Аренда {!isNotBr && <br />} коммерческих {!isNotBr && <br />}помещений
         </div>
         <TicketForm OpenModal={openModal} />
