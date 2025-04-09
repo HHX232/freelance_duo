dc90a6ba87c82e6d87370c7f46d86bd77e80c1d4
stanislavsmirnov <s.mironov@q747.tech>
2025-04-10 01:33:36 +0200

    HMR_002: верстка транспортной карты главной страницы

--- src/components/pages/map/Components/transport/Transport.tsx
+++ src/components/pages/map/Components/transport/Transport.tsx
@@ -2,10 +2,10 @@
 import {Suspense, useRef} from 'react'
 import styles from './Transport.module.scss'
 // import CornerSVG from '@icons/corner.svg'
-// import PhoneSlider from '@shared/phoneSlider/PhoneSlider'
-// import emblaStyle from '@shared/phoneSlider/embla.module.scss'
-//import clsx from 'clsx'
-// import TextAccentBlockUI from '@src/components/UI-kit/Text-Blocks/TextAccentBlockUI/TextAccentBlockUI'
+import PhoneSlider from '@shared/phoneSlider/PhoneSlider'
+import emblaStyle from '@shared/phoneSlider/embla.module.scss'
+// import clsx from 'clsx'
+import TextAccentBlockUI from '@src/components/UI-kit/Text-Blocks/TextAccentBlockUI/TextAccentBlockUI'
 import TransportMap from '@src/components/UI-kit/transportMap/transportMap'
 import {mapRoutes} from '@src/lib/utils/catalog/mapMockData'
 
@@ -48,79 +48,50 @@ import {mapRoutes} from '@src/lib/utils/catalog/mapMockData'
 // ]
 
 const TransportContent = () => {
-  // const mobileSlides: React.ReactNode[] = [
-  //   <div className={`${emblaStyle['embla__slide']} ${styles['caption-items']}`} key={666}>
-  //     <div className={`${styles['caption']}`}>
-  //       <h2 className={styles['caption__title']}>
-  //         <span>Кад</span>
-  //       </h2>
-  //       <hr className={styles['caption__divider']} />
-  //     </div>
-  //   </div>,
-  //   <div className={`${emblaStyle['embla__slide']} ${styles['caption-items']}`} key={313}>
-  //     <div className={`${styles['caption']}`}>
-  //       <h2 className={styles['caption__title']}>Кронштадтское шоссе</h2>
-  //       <hr className={styles['caption__divider']} />
-  //     </div>
-  //   </div>,
-  //   <div className={`${emblaStyle['embla__slide']} ${styles['caption-items']}`} key={777}>
-  //     <div className={`${styles['caption']}`}>
-  //       <h2 className={styles['caption__title']}>
-  //         <span>256</span> автобус
-  //       </h2>
-  //       <hr className={styles['caption__divider']} />
-  //     </div>
-  //   </div>,
-  //   <div className={`${emblaStyle['embla__slide']} ${styles['caption-items']}`} key={414}>
-  //     <div className={`${styles['caption']}`}>
-  //       <h2 className={styles['caption__title']}>
-  //         <span>382</span> автобус
-  //       </h2>
-  //       <hr className={styles['caption__divider']} />
-  //     </div>
-  //   </div>
-  // ]
+  const mobileSlides: React.ReactNode[] = [
+    <div className={`${emblaStyle['embla__slide']} ${styles['caption-items']}`} key={666}>
+      <div className={`${styles['caption']}`}>
+        <h2 className={styles['caption__title']}>
+          <span>Кад</span>
+        </h2>
+        <hr className={styles['caption__divider']} />
+      </div>
+    </div>,
+    <div className={`${emblaStyle['embla__slide']} ${styles['caption-items']}`} key={313}>
+      <div className={`${styles['caption']}`}>
+        <h2 className={styles['caption__title']}>Кронштадтское шоссе</h2>
+        <hr className={styles['caption__divider']} />
+      </div>
+    </div>,
+    <div className={`${emblaStyle['embla__slide']} ${styles['caption-items']}`} key={777}>
+      <div className={`${styles['caption']}`}>
+        <h2 className={styles['caption__title']}>
+          <span>256</span> автобус
+        </h2>
+        <hr className={styles['caption__divider']} />
+      </div>
+    </div>,
+    <div className={`${emblaStyle['embla__slide']} ${styles['caption-items']}`} key={414}>
+      <div className={`${styles['caption']}`}>
+        <h2 className={styles['caption__title']}>
+          <span>382</span> автобус
+        </h2>
+        <hr className={styles['caption__divider']} />
+      </div>
+    </div>
+  ]
   const sectionRef = useRef<HTMLDivElement | null>(null)
-
-
-  //поменял карту, слушатель не нужен
-  // const [mapIsActive, setMapIsActive] = useState(false)
-
-  // useEffect(() => {
-  //   if (typeof window === 'undefined') return
-
-  //   const action = (e: MouseEvent) => {
-  //     //@ts-ignore
-  //     if (e.target.id === 'MapWrapper') return setMapIsActive(true)
-
-  //     //@ts-ignore
-  //     if (e?.target?.localName === 'iframe') return
-  //     setMapIsActive(false)
-  //   }
-
-  //   window.addEventListener('click', action)
-
-  //   return () => {
-  //     window.removeEventListener('click', action)
-  //   }
-  // })
+  const customPoi = [{ coords: [59.999664, 29.746338], name: "Кронфорт", icon: 'kf_green_icon.svg' }]
 
   return (
     <div>
       {
         <section id='MapWrapper' className={styles.section} ref={sectionRef}>
           <div className={styles.overflow} />
-          <TransportMap customRoutes={mapRoutes} wrapperClass={styles.map} withLegend={false} withRouteButtons />
-          {/* поменял карту на наш компонент + в стиле pointer-events none убрал */}
-          {/* <iframe
-            src='https://yandex.ru/map-widget/v1/?um=constructor%3Ac0b8ef20c1a292fb67601e097be7374975c9a1f18327bc55a43516c3e5155808&amp;source=constructor'
-            className={clsx(mapIsActive ? styles.mapIsActive : '', styles.map)}
-            height='100%'
-            width='100%'
-          ></iframe> */}
-          {/* <div className={`${styles.captions} ${isVisible ? styles.visible : ''}`}> */}
-            {/* <h2 className={styles['captions-title']}>Транспортная доступность</h2> */}
-            {/* <div className={`${styles['caption-items']} ${styles['desktop_captions']}`}> */}
+          <TransportMap customRoutes={mapRoutes} customPoi={customPoi} wrapperClass={styles.map} withLegend={false} withPoi withRouteButtons />
+          <div className={`${styles.captions}`}>
+            <h2 className={styles['captions-title']}>Транспортная доступность</h2>
+            <div className={`${styles['caption-items']} ${styles['desktop_captions']}`}>
               {/* <div className={styles['caption']}>
                 <CornerSVG />
                 <h2 className={styles['caption__title']}>
@@ -128,20 +99,20 @@ const TransportContent = () => {
                 </h2>
                 <hr className={styles['caption__divider']} />
               </div> */}
-              {/* <TextAccentBlockUI
+              <TextAccentBlockUI
                 theme='dark'
                 textTitle='КАД'
                 hideSubTitle
                 hideMainContent
                 textSubTitle=''
                 textMainContent=''
-              /> */}
+              />
               {/* <div className={styles['caption']}>
                 <CornerSVG />
                 <h2 className={styles['caption__title']}>Кронштадтское шоссе</h2>
                 <hr className={styles['caption__divider']} />
               </div> */}
-              {/* <TextAccentBlockUI
+              <TextAccentBlockUI
                 theme='dark'
                 textSubTitle='Кронштадтское шоссе'
                 hideTitle
@@ -149,7 +120,7 @@ const TransportContent = () => {
                 // extraUnderlineClass={styles.under_class}
                 hideMainContent
                 textMainContent=''
-              /> */}
+              />
               {/* <div className={styles['caption']}>
                 <CornerSVG />
                 <h2 className={styles['caption__title']}>
@@ -157,13 +128,13 @@ const TransportContent = () => {
                 </h2>
                 <hr className={styles['caption__divider']} />
               </div> */}
-              {/* <TextAccentBlockUI
+              <TextAccentBlockUI
                 theme='dark'
                 textSubTitle='АВТОБУС'
                 textTitle='256'
                 hideMainContent
                 textMainContent=''
-              /> */}
+              />
               {/* <div className={styles['caption']}>
                 <CornerSVG />
                 <h2 className={styles['caption__title']}>
@@ -171,15 +142,15 @@ const TransportContent = () => {
                 </h2>
                 <hr className={styles['caption__divider']} />
               </div> */}
-              {/* <TextAccentBlockUI
+              <TextAccentBlockUI
                 theme='dark'
                 textSubTitle='АВТОБУС'
                 textTitle='382'
                 hideMainContent
                 textMainContent=''
-              /> */}
-            {/* </div> */}
-            {/* <div className={`${styles['caption-items']} ${styles['mobile_captions']} ${isVisible ? styles.visible : ''}`}>
+              />
+            </div>
+            <div className={`${styles['caption-items']} ${styles['mobile_captions']}`}>
               <PhoneSlider
                 embalaContainerClassName={styles.phone_slider_container}
                 sliderWrapperClassName={styles.phone_slider_wrapper}
@@ -187,8 +158,8 @@ const TransportContent = () => {
                 options={{dragFree: true}}
                 slides={mobileSlides}
               />
-            </div> */}
-          {/* </div> */}
+            </div>
+          </div>
         </section>
       }
     </div>
