1686a6e89f2ab7157449af491b85c16ab4a5aadd
stanislavsmirnov <s.mironov@q747.tech>
2025-04-11 03:12:53 +0200

    HMR-003: правка маршрутов карты главной страницы

--- src/components/pages/map/Components/transport/RoutesMap.tsx
+++ src/components/pages/map/Components/transport/RoutesMap.tsx
@@ -6,14 +6,23 @@ import {mapRoutes} from '@src/lib/utils/catalog/mapMockData'
 
 const TransportContent = () => {
   const sectionRef = useRef<HTMLDivElement | null>(null)
-  const customPoi = [{ coords: [59.999664, 29.746338], name: "Кронфорт", icon: 'kf_green_icon.svg' }]
+  //const customPoi = [{ coords: [59.999664, 29.746338], name: "Кронфорт", icon: 'kf_green_icon.svg' }]
+  const customState = {center: [60.052332, 29.915930], zoom: 11, controls: []};
 
   return (
     <div>
       {
         <section id='MapWrapper' className={styles.section} ref={sectionRef}>
           <div className={styles.overflow} />
-          <TransportMap customRoutes={mapRoutes} customPoi={customPoi} wrapperClass={styles.map} withLegend={false} withPoi withRouteButtons />
+          <TransportMap
+            customRoutes={mapRoutes}
+            //customPoi={customPoi}
+            customState={customState}
+            wrapperClass={styles.map}
+            withLegend={false}
+            withPoi
+            withRouteButtons
+          />
         </section>
       }
     </div>
